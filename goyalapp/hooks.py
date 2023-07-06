2ed8c1b28efb69ae8213bab29e2f6905034deef5
sumeetagra <sg@goyalironsteel.com>
2023-07-06 22:55:36 +0530

    Update hooks.py

--- goyalapp/hooks.py
+++ goyalapp/hooks.py
@@ -17,7 +17,7 @@ app_include_css = "/assets/goyalapp/css/goyalapp.css"
 # app_include_js = "/assets/goyalapp/js/goyalapp.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/goyalapp/css/goyalapp.css"
+web_include_css = "/assets/goyalapp/css/goyalapp.css"
 # web_include_js = "/assets/goyalapp/js/goyalapp.js"
 
 # include custom scss in every website theme (without file extension ".scss")
