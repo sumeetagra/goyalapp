a0052f6a5684769cb0bcb7961083f4bc6ec30aa4
sumeetagra <sg@goyalironsteel.com>
2023-07-08 09:37:32 +0530

    Update index.py

--- goyalapp/www/saleslist/index.py
+++ goyalapp/www/saleslist/index.py
@@ -5,9 +5,8 @@ from frappe import _
 def get_context(context):
 	context.no_cache = 1
 	context.show_sidebar = True
-	context.doc = frappe.get_all("Sales Invoice")
+	context.doc = frappe.get_doc(frappe.form_dict.doctype, frappe.form_dict.name)
 
-	context.enabled_checkout = frappe.get_doc("E Commerce Settings").enable_checkout
 
 	if not frappe.has_website_permission(context.doc):
 		frappe.throw(_("Not Permitted"), frappe.PermissionError)
