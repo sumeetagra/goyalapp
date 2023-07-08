a332dfe69ee57d7917b7f352c9b500964ca17d41
sumeetagra <sg@goyalironsteel.com>
2023-07-08 09:52:27 +0530

    Update index.py

--- goyalapp/www/saleslist/index.py
+++ goyalapp/www/saleslist/index.py
@@ -3,6 +3,10 @@ from frappe import _
 
 
 def get_context(context):
-	context.doc = frappe.get_doc('Sales Invoice')
+	context.doc = frappe.get_all(
+		"Sales Invoice",
+		fields=["name", "file_name", "file_url", "is_private"],
+		filters={"attached_to_name": dn, "attached_to_doctype": dt, "is_private": 0},
+	)
+
 	console.log(doc.name)
-    return context
