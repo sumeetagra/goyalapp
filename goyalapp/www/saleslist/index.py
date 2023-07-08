4e2cde32ea71d6c35ba1b9d40861a550c56ed546
sumeetagra <sg@goyalironsteel.com>
2023-07-08 12:52:21 +0530

    up

--- goyalapp/www/saleslist/index.py
+++ goyalapp/www/saleslist/index.py
@@ -3,6 +3,8 @@ from frappe import _
 
 
 def get_context(context):
+	context.no_cache = 1
+	context.show_sidebar = True
 	context.doc = frappe.get_all(
 		"Sales Invoice",
 		fields=["name", "customer"],
