82a4c4ab2eef2f9a544d62da1ecf4356f546286b
sumeetagra <sg@goyalironsteel.com>
2023-07-13 14:44:57 +0530

    Update goyalapi.py

--- goyalapp/www/goyalapi.py
+++ goyalapp/www/goyalapi.py
@@ -51,7 +51,7 @@ def get(doctype, txt=None, fields=None, filters=None, limit_start=0, limit=20, p
 	if list_context.filters:
 		filters.update(list_context.filters)
 
-	_get_list = get_list
+	_get_list = get_transaction_list
 
 	kwargs = dict(
 		doctype=doctype,
@@ -76,6 +76,68 @@ def get(doctype, txt=None, fields=None, filters=None, limit_start=0, limit=20, p
 		"SG": kwargs,
 		"SG1": filters,
 	}
+
+def get_transaction_list(
+	doctype,
+	txt=None,
+	filters=None,
+	limit_start=0,
+	limit_page_length=20,
+	order_by="modified",
+	custom=False,
+):
+	user = frappe.session.user
+	ignore_permissions = False
+
+	if not filters:
+		filters = {}
+
+	filters["docstatus"] = ["<", "2"] if doctype in ["Supplier Quotation", "Purchase Invoice"] else 1
+
+	if (user != "Guest" and is_website_user()) or doctype == "Request for Quotation":
+		parties_doctype = (
+			"Request for Quotation Supplier" if doctype == "Request for Quotation" else doctype
+		)
+		# find party for this contact
+		customers, suppliers = get_customers_suppliers(parties_doctype, user)
+
+		if customers:
+			if doctype == "Quotation":
+				filters["quotation_to"] = "Customer"
+				filters["party_name"] = ["in", customers]
+			else:
+				filters["customer"] = ["in", customers]
+		elif suppliers:
+			filters["supplier"] = ["in", suppliers]
+		elif not custom:
+			return []
+
+		if doctype == "Request for Quotation":
+			parties = customers or suppliers
+			return rfq_transaction_list(parties_doctype, doctype, parties, limit_start, limit_page_length)
+
+		# Since customers and supplier do not have direct access to internal doctypes
+		ignore_permissions = True
+
+		if not customers and not suppliers and custom:
+			ignore_permissions = False
+			filters = {}
+
+	transactions = get_list_for_transactions(
+		doctype,
+		txt,
+		filters,
+		limit_start,
+		limit_page_length,
+		fields="name",
+		ignore_permissions=ignore_permissions,
+		order_by="modified desc",
+	)
+
+	if custom:
+		return transactions
+
+	return post_process(doctype, transactions)
 
 def prepare_filters(doctype, controller, kwargs):
 	for key in kwargs.keys():
