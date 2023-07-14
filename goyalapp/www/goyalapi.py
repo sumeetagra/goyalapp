b2e2451e31ea68359e060440cab3da0998be2a12
sumeetagra <sg@goyalironsteel.com>
2023-07-14 09:40:37 +0530

    Update goyalapi.py

--- goyalapp/www/goyalapi.py
+++ goyalapp/www/goyalapi.py
@@ -56,6 +56,7 @@ def get_list(
 				filters["name"] = ("like", "%" + txt + "%")
 			else:
 				filters.append([doctype, "name", "like", "%" + txt + "%"])
+		ignore_permissions = True
 
 	return frappe.get_list(
 		doctype,
