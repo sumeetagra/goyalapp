558e73574cb514b30f971ac307f049c8a27c822b
sumeetagra <sg@goyalironsteel.com>
2023-07-16 20:27:54 +0530

    Update goyalapi.py

--- goyalapp/www/goyalapi.py
+++ goyalapp/www/goyalapi.py
@@ -80,20 +80,7 @@ def get_customers_suppliers(doctype, user):
 	customers = []
 	suppliers = []
 	meta = frappe.get_meta(doctype)
-
-	customer_field_name = get_customer_field_name(doctype)
-
-	has_customer_field = meta.has_field(customer_field_name)
-	has_supplier_field = meta.has_field("supplier")
-
-	if has_common(["Supplier", "Customer"], frappe.get_roles(user)):
-		suppliers = get_parents_for_user("Supplier")
-		customers = get_parents_for_user("Customer")
-	elif frappe.has_permission(doctype, "read", user=user):
-		customer_list = frappe.get_list("Customer")
-		customers = suppliers = [customer.name for customer in customer_list]
-
-	return customers if has_customer_field else None, suppliers if has_supplier_field else None
+	return meta
 
 
 def get_customer_field_name(doctype):
@@ -135,7 +122,7 @@ def get_transaction_list(
 		)
 
 		# find party for this contact
-		customers, suppliers = get_customers_suppliers(parties_doctype, user)
+		customers = get_customers_suppliers(parties_doctype, user)
 
 
 		# SG UPDATE
