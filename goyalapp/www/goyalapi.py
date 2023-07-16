ff4023911fdbcb9d9f8c0f69bceb61f2ad1d6e60
sumeetagra <sg@goyalironsteel.com>
2023-07-16 22:39:00 +0530

    Update goyalapi.py

--- goyalapp/www/goyalapi.py
+++ goyalapp/www/goyalapi.py
@@ -81,20 +81,6 @@ def get_customers_suppliers(doctype, user):
 	meta = frappe.get_meta(doctype)
 	return meta
 
-	customer_field_name = get_customer_field_name(doctype)
-
-	has_customer_field = meta.has_field(customer_field_name)
-	has_supplier_field = meta.has_field("supplier")
-
-	if has_common(["Supplier", "Customer"], frappe.get_roles(user)):
-		suppliers = get_parents_for_user("Supplier")
-		customers = get_parents_for_user("Customer")
-	elif frappe.has_permission(doctype, "read", user=user):
-		customer_list = frappe.get_list("Customer")
-		customers = suppliers = [customer.name for customer in customer_list]
-
-	return customers if has_customer_field else None, suppliers if has_supplier_field else None
-
 
 def get_customer_field_name(doctype):
 	if doctype == "Quotation":
