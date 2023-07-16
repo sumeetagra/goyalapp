357b25219b77915c1afdea5882f807e59197f62e
sumeetagra <sg@goyalironsteel.com>
2023-07-16 11:21:54 +0530

    Update goyalapi.py

--- goyalapp/www/goyalapi.py
+++ goyalapp/www/goyalapi.py
@@ -27,7 +27,118 @@ def get_context(context, **dict_params):
 
 
 @frappe.whitelist(allow_guest=True)
-def get_list(
+def get(
+	doctype, txt=None, limit_start=0, fields=None, cmd=None, limit=20, web_form_name=None, **kwargs
+):
+	"""Returns processed HTML page for a standard listing."""
+	limit_start = cint(limit_start)
+
+	if frappe.is_table(doctype):
+		frappe.throw(_("Child DocTypes are not allowed"), title=_("Invalid DocType"))
+
+	if not txt and frappe.form_dict.search:
+		txt = frappe.form_dict.search
+		del frappe.form_dict["search"]
+
+	controller = get_controller(doctype)
+	meta = frappe.get_meta(doctype)
+
+	filters = prepare_filters(doctype, controller, kwargs)
+	list_context = get_list_context(frappe._dict(), doctype, web_form_name)
+	list_context.title_field = getattr(controller, "website", {}).get(
+		"page_title_field", meta.title_field or "name"
+	)
+
+	if list_context.filters:
+		filters.update(list_context.filters)
+
+	_get_list = list_context.get_list or get_list
+
+	kwargs = dict(
+		doctype=doctype,
+		txt=txt,
+		filters=filters,
+		limit_start=limit_start,
+		limit_page_length=limit,
+		order_by=list_context.order_by or "modified desc",
+	)
+
+	# allow guest if flag is set
+	if not list_context.get_list and (list_context.allow_guest or meta.allow_guest_to_view):
+		kwargs["ignore_permissions"] = True
+
+	raw_result = _get_list(**kwargs)
+
+	# list context to be used if called as rendered list
+	frappe.flags.list_context = list_context
+
+	return raw_result
+
+
+def get_transaction_list(
+	doctype,
+	txt=None,
+	filters=None,
+	limit_start=0,
+	limit_page_length=20,
+	order_by="modified",
+	custom=False,
+):
+	user = frappe.session.user
+	ignore_permissions = False
+
+	if not filters:
+		filters = {}
+
+	filters["docstatus"] = ["<", "2"] if doctype in ["Supplier Quotation", "Purchase Invoice"] else 1
+
+	if (user != "Guest" and is_website_user()) or doctype == "Request for Quotation":
+		parties_doctype = (
+			"Request for Quotation Supplier" if doctype == "Request for Quotation" else doctype
+		)
+		# find party for this contact
+		customers, suppliers = get_customers_suppliers(parties_doctype, user)
+
+		if customers:
+			if doctype == "Quotation":
+				filters["quotation_to"] = "Customer"
+				filters["party_name"] = ["in", customers]
+			else:
+				filters["customer"] = ["in", customers]
+		elif suppliers:
+			filters["supplier"] = ["in", suppliers]
+		elif not custom:
+			return []
+
+		if doctype == "Request for Quotation":
+			parties = customers or suppliers
+			return rfq_transaction_list(parties_doctype, doctype, parties, limit_start, limit_page_length)
+
+		# Since customers and supplier do not have direct access to internal doctypes
+		ignore_permissions = True
+
+		if not customers and not suppliers and custom:
+			ignore_permissions = False
+			filters = {}
+
+	transactions = get_list_for_transactions(
+		doctype,
+		txt,
+		filters,
+		limit_start,
+		limit_page_length,
+		fields="name",
+		ignore_permissions=ignore_permissions,
+		order_by="modified desc",
+	)
+
+	if custom:
+		return transactions
+
+	return post_process(doctype, transactions)
+
+
+def get_list_for_transactions(
 	doctype,
 	txt,
 	filters,
@@ -37,41 +148,45 @@ def get_list(
 	fields=None,
 	order_by=None,
 ):
-	user = frappe.session.user
+	"""Get List of transactions like Invoices, Orders"""
+	from frappe.www.list import get_list
 
 	meta = frappe.get_meta(doctype)
-	if not filters:
-		filters = []
-
-	if not fields:
-		fields = "distinct *"
-
+	data = []
 	or_filters = []
 
-	if txt:
-		if meta.search_fields:
-			for f in meta.get_search_fields():
-				if f == "name" or meta.get_field(f).fieldtype in ("Data", "Text", "Small Text", "Text Editor"):
-					or_filters.append([doctype, f, "like", "%" + txt + "%"])
-		else:
-			if isinstance(filters, dict):
-				filters["name"] = ("like", "%" + txt + "%")
-			else:
-				filters.append([doctype, "name", "like", "%" + txt + "%"])
-		ignore_permissions = True
-
-	raw_result= frappe.get_list(
+	for d in get_list(
 		doctype,
-		fields=fields,
+		txt,
 		filters=filters,
-		or_filters=or_filters,
+		fields="name",
 		limit_start=limit_start,
 		limit_page_length=limit_page_length,
 		ignore_permissions=ignore_permissions,
-		order_by=order_by,
-	)
-	return 
-	{
-		"SG2": raw_result,
-}
+		order_by="modified desc",
+	):
+		data.append(d)
+
+	if txt:
+		if meta.get_field("items"):
+			if meta.get_field("items").options:
+				child_doctype = meta.get_field("items").options
+				for item in frappe.get_all(child_doctype, {"item_name": ["like", "%" + txt + "%"]}):
+					child = frappe.get_doc(child_doctype, item.name)
+					or_filters.append([doctype, "name", "=", child.parent])
+
+	if or_filters:
+		for r in frappe.get_list(
+			doctype,
+			fields=fields,
+			filters=filters,
+			or_filters=or_filters,
+			limit_start=limit_start,
+			limit_page_length=limit_page_length,
+			ignore_permissions=ignore_permissions,
+			order_by=order_by,
+		):
+			data.append(r)
+
+	return data
 
