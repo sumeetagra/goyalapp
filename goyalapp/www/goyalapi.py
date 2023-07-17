98c6b0c8818fddfb75de95adbb841e2e6568d82c
sumeetagra <sg@goyalironsteel.com>
2023-07-17 22:48:45 +0530

    Update goyalapi.py

--- goyalapp/www/goyalapi.py
+++ goyalapp/www/goyalapi.py
@@ -83,10 +83,9 @@ def get_customers_suppliers(doctype, user):
 	has_customer_field = meta.has_field(customer_field_name)
 	has_supplier_field = meta.has_field("supplier")
 
-if frappe.has_permission(doctype, "read", user=user):
 		customer_list = frappe.get_list("Customer")
 		customers = suppliers = [customer.name for customer in customer_list]
-		
+
 	return customers if has_customer_field else None, suppliers if has_supplier_field else None
 
 
