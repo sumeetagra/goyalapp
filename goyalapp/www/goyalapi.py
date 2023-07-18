9eee2512735b8d9eb73c658398f38655ce8b1695
sumeetagra <sg@goyalironsteel.com>
2023-07-19 00:11:00 +0530

    Update goyalapi.py

--- goyalapp/www/goyalapi.py
+++ goyalapp/www/goyalapi.py
@@ -50,7 +50,7 @@ def get(
 		filters = json.loads(frappe.local.form_dict["filters"])
 
 #	filters = prepare_filters(doctype, controller, kwargs)
-	return filters
+#	return filters
 
 	_get_list = get_transaction_list
 
@@ -105,7 +105,8 @@ def get_transaction_list(
 		elif not custom:
 			return []
 
-	#	filters["posting_date"] = ["between", ("2021-09-21", "2021-09-23")]
+		filters["posting_date"] = ["between", ("2021-09-21", "2021-09-23")]
+		return filters
 
 		# Since customers and supplier do not have direct access to internal doctypes
 		ignore_permissions = True
