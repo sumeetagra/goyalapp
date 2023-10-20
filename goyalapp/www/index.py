8f34bd03849aae24abc1fac6cc4e8897aef87d6b
sumeetagra <sg@goyalironsteel.com>
2023-10-20 22:49:44 +0530

    Update index.py

--- goyalapp/www/index.py
+++ goyalapp/www/index.py
@@ -9,6 +9,8 @@ no_cache = 1
 
 
 def get_context(context):
+	if frappe.session.user != "Guest":
+		context.current_user = frappe.get_doc("User", frappe.session.user)
 	context.show_sidebar = False
 	context.full_width = True
 	context.only_static = False
