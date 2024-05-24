34aa8a308720b38ad2d3ec02c2713ef4622069d0
sumeetagra <sg@goyalironsteel.com>
2024-05-24 16:18:17 +0530

    Update goyalapi.py

--- goyalapp/www/goyalapi.py
+++ goyalapp/www/goyalapi.py
@@ -301,7 +301,6 @@ def get_list_SG_transactions(
 	fields=None,
 	order_by=None,
 ):
-	return "SG IS HERE!"
 
 	"""Get List of transactions like Invoices, Orders"""
 	from frappe.www.list import get_list
