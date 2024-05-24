6a0fe5fd805ae06b4172d022594d1d6d08308961
sumeetagra <sg@goyalironsteel.com>
2024-05-24 13:20:27 +0530

    Update goyalapi.py

--- goyalapp/www/goyalapi.py
+++ goyalapp/www/goyalapi.py
@@ -92,6 +92,72 @@ def GetSupplierBills(doctype, StartDate, EndDate, txt=None, filters=None, limit_
 	}
 
 
+def GetCustomerBills(doctype, StartDate, EndDate, txt=None, filters=None, limit_start=0, limit_page_length=20, order_by="creation", custom=False):
+
+	user = frappe.session.user
+	ignore_permissions = False
+
+	if StartDate == '':
+		frappe.throw(_("Please Enter Start Date."))
+
+	if EndDate == '':
+		frappe.throw(_("Please Enter End Date."))
+
+	if StartDate > EndDate:
+		frappe.throw(_("Start Date shall be before the End Date."))
+
+	if not filters:
+		filters = {}
+
+	filters["docstatus"] = ["<", "2"] if doctype in ["Supplier Quotation", "Purchase Invoice"] else 1
+
+	filters["posting_date"] = ["between", [StartDate, EndDate]]
+
+	from erpnext.controllers.website_list_for_contact import get_customers_suppliers
+
+	if (user != "Guest" and is_website_user()) or doctype == "Request for Quotation":
+		parties_doctype = "Request for Quotation Supplier" if doctype == "Request for Quotation" else doctype
+		# find party for this contact
+		customers, suppliers = get_customers_suppliers(parties_doctype, user)
+
+		if customers:
+			if doctype == "Quotation":
+				filters["quotation_to"] = "Customer"
+				filters["party_name"] = ["in", customers]
+			else:
+				filters["customer"] = ["in", customers]
+		elif suppliers:
+			filters["supplier"] = ["in", suppliers]
+		elif not custom:
+			return []
+
+		if doctype == "Request for Quotation":
+			parties = customers or suppliers
+			return rfq_transaction_list(parties_doctype, doctype, parties, limit_start, limit_page_length)
+
+		# Since customers and supplier do not have direct access to internal doctypes
+		ignore_permissions = True
+
+		if not customers and not suppliers and custom:
+			ignore_permissions = False
+			filters = {}
+
+	transactions = get_list_SG_transactions(
+		doctype,
+		txt,
+		filters,
+		limit_start,
+		limit_page_length,
+		fields="name",
+		ignore_permissions=ignore_permissions,
+		order_by="creation desc",
+	)
+
+	return {
+		"DataResponse": transactions
+	}
+
+
 def get_list_SG_transactions(
 	doctype,
 	txt,
