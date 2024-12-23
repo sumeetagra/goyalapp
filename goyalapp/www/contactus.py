e975a6ba0178208a83dec5d2643a9dbfee466bf3
sumeetagra <sg@goyalironsteel.com>
2024-12-23 18:18:30 +0530

    Update contactus.py

--- goyalapp/www/contactus.py
+++ goyalapp/www/contactus.py
@@ -7,7 +7,6 @@ from frappe import _
 
 no_cache = 1
 
-
 def get_context(context):
 	if frappe.session.user == "Guest":
 		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
