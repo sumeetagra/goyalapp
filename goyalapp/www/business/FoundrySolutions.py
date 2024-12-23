32649f9000321811bded5c12cf0c3f25b177bf22
sumeetagra <sg@goyalironsteel.com>
2024-12-23 17:27:16 +0530

    d

---
+++ goyalapp/www/business/FoundrySolutions.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: MIT. See LICENSE
+
+import frappe
+import frappe.www.list
+from frappe import _
+
+no_cache = 1
+
+
+def get_context(context):
+	if frappe.session.user != "Guest":
+		context.current_user = frappe.get_doc("User", frappe.session.user)
+	context.show_sidebar = False
+	context.full_width = True
+	context.only_static = False
