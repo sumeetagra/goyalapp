8a0d1e0ba30267b83793aa7b67cd9b90ec28f53b
sumeetagra <sg@goyalironsteel.com>
2025-01-06 11:32:50 +0530

    Create goyalwebsite.py

---
+++ goyalapp/controllers/goyalwebsite.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+
+import copy
+import json
+
+import frappe
+from frappe import _
+from frappe.utils import cstr, flt
