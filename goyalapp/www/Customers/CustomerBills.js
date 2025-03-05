546b9e3e718ae63612b03fb3a83c4a502afaa077
sumeetagra <sg@goyalironsteel.com>
2025-03-05 12:09:13 +0530

    Update CustomerBills.js

--- goyalapp/www/Customers/CustomerBills.js
+++ goyalapp/www/Customers/CustomerBills.js
@@ -83,7 +83,7 @@ function setup_details_page() {
 			callback: (response) => {
 				alert(JSON.stringify(response.message.DataResponse[0]));
 				let data = response.message;
-					$("<p>test</p>").appendTo(".content-wrapper");
+					$("<p>test</p>").appendTo(".website-list");
 
 					$(data).appendTo(result_wrapper);
 //					$.each(data.DataResponse, function(d) {
